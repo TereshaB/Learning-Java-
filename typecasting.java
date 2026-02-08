406b59e8cdad7df7aab3cf94246880c775b13e60
TereshaB <tereshabhatt@gmail.com>
2026-02-08 18:04:06 +0545

    prac..

---
+++ typecasting.java
@@ -0,0 +1,9 @@
+public class typecasting {
+    static void main(String[] args)
+    {
+        float a= 26.66f;
+        int b = (int) a; // typecasting 
+        System.out.println(b);
+
+    }
+}
---
+++ typeconversion.java
@@ -0,0 +1,9 @@
+public class typeconversion
+ {
+    public static void main(String[] args)
+    {
+        int a= 45;
+        float b = a ; // implicit or lossless conversion
+        System.out.println(b);
+    }
+}
