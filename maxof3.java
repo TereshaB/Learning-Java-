9143aadaf4179d1d654bfbad2f78d4d67517a8a6
TereshaB <tereshabhatt@gmail.com>
2026-02-08 09:16:18 +0545

    practice

---
+++ maxof3.java
@@ -0,0 +1,31 @@
+public class maxof3 {
+   public static void main(String[]args ) 
+   {
+        int a= 5;
+        int b=10;
+        int c=15;
+if(a>b)
+        {
+            if(a>c)
+            {
+                System.out.println("a is the greatest");
+            }
+            else
+            {
+               System.out.println("c is the greatest"); 
+            }
+        }
+else 
+        {
+            if(b>c)
+            {
+                 System.out.println("b is the greatest");
+            }
+            else 
+            {
+                 System.out.println("c is the greatest"); 
+            }
+
+            }
+   }
+}
