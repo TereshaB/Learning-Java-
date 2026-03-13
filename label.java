ebb74a789b53a2ab5332790c24d7f95471ac9f3d
TereshaB <tereshabhatt@gmail.com>
2026-03-13 16:07:17 +0545

    update label class to set JLabel properties and adjust JFrame size

--- label.java
+++ label.java
@@ -2,8 +2,16 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 public class label 
 {
+public static void main(String[]args)
+{
     JLabel label =new JLabel();
+    label.setText("Where does this show up?");
+    label.setHorizontalTextPosition(JLabel.CENTER);
+    label.setVerticalTextPosition(JLabel.TOP);
     JFrame frame = new JFrame();
-    frame.setSize(300,400);
+    frame.setSize(500,500);
     frame.setVisible(true);
+    frame.add(label);
+   
+}
 }
