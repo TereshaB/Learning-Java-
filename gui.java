e87529b6cd77314fde7128def2f218afcc431be5
TereshaB <tereshabhatt@gmail.com>
2026-03-13 10:33:23 +0545

    refactor gui class to set JFrame properties and title

--- gui.java
+++ gui.java
@@ -1,11 +1,14 @@
+
 import javax.swing.JFrame;
 public class gui
 {
     public static void main(String[]args)
     {
-        JFrame frame = new JFrame();
-        frame.setSize(800,900);
-        frame.setTitle("where does this go?");
-        frame.setVisible(true);
+        JFrame frame = new JFrame(); // creates a gui frame 
+        frame.setSize(800,900); // sets size of the frame 
+        frame.setTitle("TereshaB"); // set the title for JFrame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // make the exit button actually work 
+        frame.setResizable(false); // the JFrame isn't resizable
+        frame.setVisible(true); // JFrame is made visible 
     }
 }
