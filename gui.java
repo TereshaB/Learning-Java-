08b57648b745e3b1c01af89a898212295f778ed5
TereshaB <tereshabhatt@gmail.com>
2026-03-13 10:17:55 +0545

    update

---
+++ gui.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+public class gui
+{
+    public static void main(String[]args)
+    {
+        JFrame frame = new JFrame();
+        frame.setSize(800,900);
+        frame.setVisible(true);
+    }
+}
